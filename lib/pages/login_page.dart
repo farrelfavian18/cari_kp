7599ffaa27341706d40085fa313aaef086b7062d
farrelfavian18 <farrelfavian180502@mhs.mdp.ac.id>
2024-01-25 04:08:56 +0700

    login signup
    blm selesai ckny

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,86 +1,85 @@
+import 'package:cari_kp/components/my_textfield.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  LoginPage({super.key});
+
+  //text editing controller
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.grey[300],
-        body: SafeArea(
-          child: Center(
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 50,
-                ),
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 50,
+              ),
 
-                //TODO logo
-                const Icon(
-                  Icons.cookie,
-                  size: 100,
-                ),
-                const SizedBox(height: 50),
+              //TODO logo
+              const Icon(
+                Icons.cookie,
+                size: 100,
+              ),
+              const SizedBox(height: 50),
 
-                //TODO welcomeback text
-                Text(
-                  'Selamat datang di Cari KP!',
-                  style: TextStyle(
-                    color: Colors.grey[700],
-                    fontSize: 16,
-                  ),
+              //TODO welcomeback text
+              Text(
+                'Selamat datang di Cari KP!',
+                style: TextStyle(
+                  color: Colors.grey[700],
+                  fontSize: 16,
                 ),
+              ),
 
-                const SizedBox(height: 50),
-                //TODO username field
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      enabledBorder: const OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white70),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey.shade400),
-                      ),
-                      fillColor: Colors.grey.shade200,
-                      filled: true,
+              const SizedBox(height: 30),
+              //TODO username field
+              MyTextField(
+                controller: usernameController,
+                hintText: 'Username',
+                obscureText: false,
+              ),
+
+              const SizedBox(height: 10),
+
+              //TODO password field
+              MyTextField(
+                controller: passwordController,
+                hintText: 'Password',
+                obscureText: true,
+              ),
+
+              const SizedBox(height: 10),
+              //TODO lupa password?
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Lupa Passowrd?',
+                      style: TextStyle(color: Colors.grey[600]),
                     ),
-                  ),
+                  ],
                 ),
+              ),
 
-                const SizedBox(height: 25),
+              const SizedBox(height: 30),
+              //TODO tombol sign in
 
-                //TODO password field
+              //TODO continue with ->
 
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      enabledBorder: const OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white70),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey.shade400),
-                      ),
-                      fillColor: Colors.grey.shade200,
-                      filled: true,
-                    ),
-                  ),
-                ),
+              //TODO google sign in buttons
 
-                //TODO lupa password?
-
-                //TODO tombol sign in
-
-                //TODO continue with ->
-
-                //TODO google sign in buttons
-
-                //TODO Register???
-              ],
-            ),
+              //TODO Register???
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
